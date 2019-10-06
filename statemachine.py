419f465cb178d8abc6b1f13f4b7ad8f0166f772b
Tom Catshoek <t.w.j.catshoek@student.tudelft.nl>
2019-10-06 18:05:27 +0200

    Auto gather statemachine alphabet

--- statemachine.py
+++ statemachine.py
@@ -21,26 +21,64 @@ class State:
             raise Exception(f'Invalid action {action} from state {self.name}')
 
 class StateMachine:
-    def __init__(self, initial_state):
+    def __init__(self, initial_state, accepting_states):
+        self.initial_state = initial_state
         self.state = initial_state
 
+        if not isinstance(accepting_states, Iterable):
+            accepting_states = [accepting_states]
+        self.accepting_states = accepting_states
+
+    # Traverses all states and collects all possible actions (i.e. the alphabet of the language)
+    def gather_alphabet(self):
+        actions = []
+        to_visit = [self.initial_state]
+        visited = []
+
+        while len(to_visit) > 0:
+            cur_state = to_visit.pop()
+            visited.append(cur_state)
+
+            for action, other_state in cur_state.edges.items():
+                actions.append(action)
+                if other_state not in visited:
+                    to_visit.append(other_state)
+
+        return set(actions)
+
+    # Runs the given inputs on the state machine
     def process_input(self, inputs):
         if not isinstance(inputs, Iterable):
             inputs = [inputs]
 
         for input in inputs:
-            nextstate = self.state.next(input)
-            print(f'({self.state.name}) ={input}=> ({nextstate.name})')
-            self.state = nextstate
+            try:
+                nextstate = self.state.next(input)
+                print(f'({self.state.name}) ={input}=> ({nextstate.name})')
+                self.state = nextstate
+            except Exception as e:
+                print(e)
+                return False
 
-s1 = State(1)
-s2 = State(2)
-s1.add_edge('to2', s2)
-s2.add_edge('to1', s1)
+        return self.state in self.accepting_states
 
-sm = StateMachine(s1)
+    def reset(self):
+        self.state = self.initial_state
 
-try:
-    sm.process_input(['to2', 'to1', 'to2', 'to2'])
-except Exception as e:
-    print(e)
+
+if __name__ == "__main__":
+    s1 = State(1)
+    s2 = State(2)
+    s3 = State(3)
+
+    s1.add_edge('to2', s2)
+    s2.add_edge('to3', s3)
+    s3.add_edge('to1', s1)
+
+    sm = StateMachine(s1, s3)
+
+    print(sm.gather_alphabet())
+
+    accepted = sm.process_input(['to2', 'to3', 'to1', 'to2', 'to3'])
+
+    print(f'Accepted: {accepted}')
