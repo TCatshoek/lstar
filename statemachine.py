e11dafb39c489fbaf4eca2adeac58f75be1b029e
Tom Catshoek <t.w.j.catshoek@student.tudelft.nl>
2019-10-07 19:49:09 +0200

    str() the states and statemachine

--- statemachine.py
+++ statemachine.py
@@ -8,6 +8,9 @@ class State:
         self.name = name
         self.edges = edges
 
+    def __str__(self):
+        return f'[State: {self.name}, edges: {[f"{a}:{n.name}" for a, n in self.edges.items()]}]'
+
     def add_edge(self, action, other_state):
         if action not in self.edges.keys():
             self.edges[action] = other_state
@@ -28,6 +31,24 @@ class StateMachine:
         if not isinstance(accepting_states, Iterable):
             accepting_states = [accepting_states]
         self.accepting_states = accepting_states
+
+    def __str__(self):
+        to_visit = [self.initial_state]
+        visited = []
+
+        while len(to_visit) > 0:
+            cur_state = to_visit.pop()
+            if cur_state not in visited:
+                visited.append(cur_state)
+
+            for action, other_state in cur_state.edges.items():
+                if other_state not in visited:
+                    to_visit.append(other_state)
+
+        #Hacky backslash thing
+        tab = '\t'
+        nl = '\n'
+        return f'[StateMachine: \n { nl.join([f"{tab}{str(state)}" for state in visited]) } \n]'
 
     # Traverses all states and collects all possible actions (i.e. the alphabet of the language)
     def gather_alphabet(self):
@@ -54,10 +75,10 @@ class StateMachine:
         for input in inputs:
             try:
                 nextstate = self.state.next(input)
-                print(f'({self.state.name}) ={input}=> ({nextstate.name})')
+                #print(f'({self.state.name}) ={input}=> ({nextstate.name})')
                 self.state = nextstate
             except Exception as e:
-                print(e)
+                #print(e)
                 return False
 
         return self.state in self.accepting_states
