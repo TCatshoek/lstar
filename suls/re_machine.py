56ae102fa0b5f86694887706386efabde589190f
Tom Catshoek <t.w.j.catshoek@student.tudelft.nl>
2019-10-09 16:59:16 +0200

    Allow specifying SUT using regular expressions

---
+++ suls/re_machine.py
@@ -0,0 +1,34 @@
+from suls.sul import SUL
+from typing import Iterable
+from functools import reduce
+import re
+
+
+class RegexMachine(SUL):
+    def __init__(self, expression):
+        self.expression = expression
+
+    def process_input(self, inputs):
+        if not isinstance(inputs, Iterable):
+            inputs = [inputs]
+
+        # Get string representation of the input sequence
+        if inputs == tuple():
+            input = ''
+        else:
+            input = reduce(lambda x, y: str(x) + str(y), inputs)
+
+        match = re.search(self.expression, input)
+
+        if match is not None:
+            return match.group() == input
+        else:
+            return False
+
+    def reset(self):
+        pass
+
+    # assume the alphabet is all alphabetical characters in the regex
+    def get_alphabet(self):
+        pattern = re.compile('[^a-zA-Z]')
+        return set(pattern.sub('', self.expression))
