67e478958c10daa0af2e572504cb33e3dd027b95
Tom Catshoek <t.w.j.catshoek@student.tudelft.nl>
2019-11-11 16:28:47 +0100

    clean up

--- main.py
+++ main.py
@@ -1,53 +1,27 @@
 from learners.dfalearner import DFALearner
-from equivalencecheckers.equivalencechecker import BFEquivalenceChecker
-from suls.statemachine import State, StateMachine
+from equivalencecheckers.equivalencechecker import BFEquivalenceChecker, RandomWalkEquivalenceChecker
+#from equivalencecheckers.rers_checker import BFEquivalenceChecker
+from learners.mealylearner import MealyLearner
+from suls.dfa import State, DFA
 from suls.re_machine import RegexMachine
+from suls.rersconnector import StringRERSConnector
 from teachers.teacher import Teacher
 
-# # Set up a simple state machine (S1) =a> (S2) =b> ((S3))
-# s1 = State('s1')
-# s2 = State('s2')
-# s3 = State('s3')
-#
-# s1.add_edge('a', s2)
-# s2.add_edge('b', s3)
-#
-# sm = StateMachine(s1, [s3])
-#
-# # Since we are learning a DFA, we need edges for the whole alphabet in every state
-# s1.add_edge('b', s1)
-# s2.add_edge('a', s2)
-# s3.add_edge('a', s3)
-# s3.add_edge('b', s3)
-#
-# # We are using the brute force equivalence checker
-# eqc = BFEquivalenceChecker(sm)
-#
-# # Set up the teacher, with the system under learning and the equivalence checker
-# teacher = Teacher(sm, eqc)
-#
-# # Set up the learner who only talks to the teacher
-# learner = DFALearner(teacher)
-#
-# # Get the learners hypothesis
-# hyp = learner.run()
-#
-# print('ACTUAL', sm)
-
-# Set up a SUT using regex
-sm = RegexMachine('(bb)*(aa)*(bb)*')
+# Try to learn a state machine for one of the RERS problems
+sm = StringRERSConnector('rers/TrainingSeqReachRers2019/Problem11/Problem11')
 
 # We are using the brute force equivalence checker
 eqc = BFEquivalenceChecker(sm, max_depth=15)
+#eqc = RandomWalkEquivalenceChecker(sm, max_depth=30, num_samples=100000)
 
 # Set up the teacher, with the system under learning and the equivalence checker
 teacher = Teacher(sm, eqc)
 
 # Set up the learner who only talks to the teacher
-learner = DFALearner(teacher)
+learner = MealyLearner(teacher)
 
 # Get the learners hypothesis
-hyp = learner.run()
+hyp = learner.run(show_intermediate=True)
 
 print('ACTUAL', sm)
 
