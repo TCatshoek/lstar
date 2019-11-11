f0cc480ca358b11049ad43e702f846edf382ee6e
Tom Catshoek <t.w.j.catshoek@student.tudelft.nl>
2019-11-11 16:28:05 +0100

    add examples for dfa and mealy learning

---
+++ examples/learn_dfa/learn_dfa.py
@@ -0,0 +1,41 @@
+from equivalencecheckers.equivalencechecker import BFEquivalenceChecker
+from learners.dfalearner import DFALearner
+from suls.re_machine import RegexMachine
+from suls.dfa import State, DFA
+from teachers.teacher import Teacher
+
+# Set up a simple state machine (S1) =a> (S2) =b> ((S3))
+
+s1 = State('s1')
+s2 = State('s2')
+s3 = State('s3')
+
+s1.add_edge('a', s2)
+s2.add_edge('b', s3)
+
+sm = DFA(s1, [s3])
+
+# Since we are learning a DFA, we need edges for the whole alphabet in every state
+s1.add_edge('b', s1)
+s2.add_edge('a', s2)
+s3.add_edge('a', s3)
+s3.add_edge('b', s3)
+
+# Or use a regex to define the state machine
+# This is not the same as the above example since it *only* accepts the input string 'ab'
+# and does not have the connections looping to their own state
+#sm = RegexMachine('ab')
+
+# We are using the brute force equivalence checker
+eqc = BFEquivalenceChecker(sm)
+
+# Set up the teacher, with the system under learning and the equivalence checker
+teacher = Teacher(sm, eqc)
+
+# Set up the learner who only talks to the teacher
+learner = DFALearner(teacher)
+
+# Get the learners hypothesis
+hyp = learner.run()
+
+hyp.render_graph('dfa')
---
+++ examples/learn_mealy/mealy.py
@@ -0,0 +1,30 @@
+from equivalencecheckers.equivalencechecker import BFEquivalenceChecker
+from learners.mealylearner import MealyLearner
+from suls.mealymachine import MealyState, MealyMachine
+from teachers.teacher import Teacher
+
+# Set up an example mealy machine
+s1 = MealyState('1')
+s2 = MealyState('2')
+s3 = MealyState('3')
+
+s1.add_edge('a', 'nice', s2)
+s1.add_edge('b', 'B', s1)
+s2.add_edge('a', 'nice', s3)
+s2.add_edge('b', 'back', s1)
+s3.add_edge('a', 'A', s3)
+s3.add_edge('b', 'back', s1)
+
+mm = MealyMachine(s1)
+
+# Use the brute force equivalence checker
+eqc = BFEquivalenceChecker(mm)
+
+teacher = Teacher(mm, eqc)
+
+# We are learning a mealy machine
+learner = MealyLearner(teacher)
+
+hyp = learner.run()
+
+hyp.render_graph('mealy')
