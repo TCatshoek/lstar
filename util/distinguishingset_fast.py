e1167f88565b2e1dcd2e0596c4b1dcbb5d5fe339
Tom Catshoek <t.w.j.catshoek@student.tudelft.nl>
2020-05-15 22:27:49 +0200

    Mealy generator

--- util/distinguishingset_fast.py
+++ util/distinguishingset_fast.py
@@ -6,58 +6,28 @@ from graphviz import Digraph
 import tempfile
 
 from util.dotloader import load_mealy_dot
-from itertools import product, combinations
+from itertools import product
 
 PState = namedtuple('PState', 'original current')
 
 
 # WARNING: This code is ultra jank but it works on all tested (HUGE) mealy machines as long as they are minimal
 class PartitionNode:
-    def __init__(self, states, alphabet, ptree):
+    def __init__(self, states, alphabet, acceptable=False, stable=False):
         self.states = states
         self.A = alphabet
         self.splitter = None
         self.children = []
-        self.ptree = ptree
 
-        self.stable = None
-        self.acceptable = None
-
-    def is_leaf(self):
-        return len(self.children) == 0
-
-    def is_acceptable(self):
-        acceptable = True
-        for x, y in combinations(self.states, r=2):
-            for a in self.A:
-                x_next, x_out = x.next(a)
-                y_next, y_out = y.next(a)
-                acceptable &= x_out == y_out
-        return acceptable
-
-    def is_stable(self):
-        stable = True
-        acceptable = True
-
-        for x, y in combinations(self.states, r=2):
-            for a in self.A:
-                x_next, x_out = x.next(a)
-                y_next, y_out = y.next(a)
-
-                x_next_partition = self.ptree.get_partition(x_next)
-                y_next_partition = self.ptree.get_partition(y_next)
-
-                acceptable &= x_out == y_out
-                stable &= x_next_partition == y_next_partition
-
-        return stable and acceptable
+        self.acceptable = acceptable
+        self.stable = stable
+        self.isleaf = True
 
     def split_output(self):
         #print('trying split')
         n_partitions = 0
         potential_children = {}
 
-        # Find a splitter
         for a in self.A:
             cur_seen_outputs = set()
             potential_children.clear()
@@ -79,19 +49,26 @@ class PartitionNode:
                 break
 
         if n_partitions < 2:
+            #print('Made acceptable')
+            self.acceptable = True
             return None, None
 
+        #print('found split', self.splitter, potential_children)
 
         for output, states in potential_children.items():
-            self.children.append(PartitionNode(states, self.A, self.ptree))
+            self.children.append(PartitionNode(states, self.A, self.acceptable, self.stable))
+
+        self.isleaf = len(self.children) == 0
 
         return self.splitter, self.children
 
-    def split_state(self):
+    def split_state(self, ptree):
         name = " ".join([state.name for state in self.states])
         #print("Split-stating ", " ".join([state.name for state in self.states]), "leaf:", self.isleaf, 'acceptable:', self.acceptable, 'stable:', self.stable)
 
         if len(self.states) == 1:
+            self.stable = True
+            self.acceptable = True
             return None, None
 
         # Find split using state after a
@@ -107,7 +84,7 @@ class PartitionNode:
             for cur_state in self.states:
                 next_state, output = cur_state.next(a)
                 seen_successor_states.add(next_state.name)
-                seen_partitions.add(self.ptree.get_partition(next_state))
+                seen_partitions.add(ptree.get_partition(next_state))
                 next_states_set.add(next_state)
 
             if len(seen_partitions) > 1:
@@ -115,9 +92,11 @@ class PartitionNode:
                 break
 
         if self.splitter is None:
+            #print("no splitter :(")
+            self.stable = False
             return None, None
 
-        lca = self.ptree.get_lca(next_states_set)
+        lca = ptree.get_lca(next_states_set)
         assert lca.splitter is not None
 
         self.splitter = self.splitter + lca.splitter
@@ -136,7 +115,9 @@ class PartitionNode:
                 children[next_out] = [state]
 
         for output, states in children.items():
-            self.children.append(PartitionNode(states, self.A, self.ptree))
+            self.children.append(PartitionNode(states, self.A, self.acceptable, self.stable))
+
+        self.isleaf = False
 
         return self.splitter, self.children
 
@@ -147,7 +128,7 @@ class PartitionTree:
         self.A = fsm.get_alphabet()
 
         self.states = self.fsm.get_states()
-        self.root = PartitionNode(self.states, self.A, self)
+        self.root = PartitionNode(self.states, self.A)
 
         self.nodes = [self.root]
 
@@ -159,15 +140,15 @@ class PartitionTree:
     def is_acceptable(self):
         is_acc = True
         for node in self.nodes:
-            if node.is_leaf():
-                is_acc &= node.is_acceptable()
+            if node.isleaf:
+                is_acc &= node.acceptable
         return is_acc
 
     def is_stable(self):
         is_stab = True
         for node in self.nodes:
-            if node.is_leaf():
-                is_stab &= node.is_stable()
+            if node.isleaf:
+                is_stab &= node.stable
         return is_stab
 
     def get_leaves(self):
@@ -199,7 +180,7 @@ class PartitionTree:
 
     def build(self):
         while not self.is_acceptable():
-            cur_nodes = filter(lambda x: x.is_leaf() and not x.is_acceptable(), self.nodes.copy())
+            cur_nodes = filter(lambda x: x.isleaf, self.nodes.copy())
             for node in cur_nodes:
                 splitter, children = node.split_output()
                 if splitter is not None:
@@ -212,9 +193,9 @@ class PartitionTree:
 
         while not self.is_stable():
             #print('attempting to stabilize')
-            cur_nodes = filter(lambda x: x.is_leaf() and not x.is_stable(), self.nodes.copy())
+            cur_nodes = filter(lambda x: x.isleaf and not x.stable, self.nodes.copy())
             for node in cur_nodes:
-                splitter, children = node.split_state()
+                splitter, children = node.split_state(self)
                 if splitter is not None:
                     self.solution.add(splitter)
                 if children is not None:
@@ -243,16 +224,6 @@ class PartitionTree:
 
 
 def check_distinguishing_set(fsm, dset):
-    outputs = get_dset_outputs(fsm, dset)
-
-    if len(set(outputs.values())) < len(outputs):
-        print("Not unique", outputs)
-        return False
-    else:
-        print('succes!', len(outputs), 'states,', len(set(outputs)), 'unique outputs')
-        return True
-
-def get_dset_outputs(fsm, dset):
     states = fsm.get_states()
     outputs = {}
     for state in states:
@@ -262,7 +233,13 @@ def get_dset_outputs(fsm, dset):
             out.append(mm.process_input(dseq))
             mm.reset()
         outputs[state] = tuple(out.copy())
-    return outputs
+
+    if len(set(outputs.values())) < len(outputs):
+        print("Not unique", outputs)
+        return False
+    else:
+        print('succes!', len(outputs), 'states,', len(set(outputs)), 'unique outputs')
+        return True
 
 # Uses partition refinement to find a distinguishing set for a given mealy machine
 def get_distinguishing_set(fsm: MealyMachine):
@@ -271,15 +248,14 @@ def get_distinguishing_set(fsm: MealyMachine):
 
     dset = ptree.solution
 
-    #check_distinguishing_set(fsm, dset)
+    assert check_distinguishing_set(fsm, dset)
 
     return ptree.solution
 
 
-import pickle
 import time
 if __name__ == '__main__':
-    path = "/home/tom/projects/lstar/rers/industrial/m34.dot"
+    path = "/home/tom/projects/lstar/rers/industrial/m27.dot"
 
     fsm = load_mealy_dot(path)
 
@@ -288,3 +264,4 @@ if __name__ == '__main__':
     t1 = time.time()
 
     print(t1 - t0)
+
